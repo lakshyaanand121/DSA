2017ad968b0b9be2656acaefe8c0380841efc44f
lakshyaanand121 <lakshyaanand121@gmail.com>
2026-07-29 21:09:31 +0530

    Add files via upload
    array-insertion

---
+++ array4.cpp
@@ -0,0 +1,31 @@
+// insertion
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n, a[10], loc, item;
+    cout << "enter number of element in array:\n"
+         << endl;
+    cin >> n;
+    cout << "enter the elements:\n"
+         << endl;
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    cout << "enter the location and the item to enter: \n"
+         << endl;
+    cin >> loc >> item;
+    for (i = n - 1; i >= loc - 1; i--)
+    {
+        a[i + 1] = a[i];
+    }
+    a[loc - 1] = item;
+    n = n + 1;
+    cout << "after insertion" << endl;
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
